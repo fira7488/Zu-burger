501878ddcca5b6fe15912a932dd2fdc48be291d2
faruq Arse <firvpl25@gmail.com>
2026-06-10 14:18:21 +0300

    fix: wrap app with LanguageProvider so translations work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
+import LanguageProvider from "./i18n";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <AppRoutes />
-      </CartProvider>
-    </AuthProvider>
+    <LanguageProvider>
+      <AuthProvider>
+        <CartProvider>
+          <AppRoutes />
+        </CartProvider>
+      </AuthProvider>
+    </LanguageProvider>
   );
 }
